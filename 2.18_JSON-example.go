ed7c9adce08ac3fb2b1dce162135bb22a4b85bb9
Chris Wood <cjwood032@gmail.com>
2023-10-28 21:00:03 -0400

    Basic JSON example

---
+++ 2.18_JSON-example.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+)
+
+type Person struct {
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	HasDog    bool   `json:"has_dog"`
+}
+
+func main() {
+	myJson := `
+	[
+		{
+			"first_name": "Clark",
+			"last_name": "Kent",
+			"has_dog": true
+		},
+		{
+		"first_name": "Bruce",
+		"last_name": "Wayne",
+		"has_dog": false
+		}
+	]`
+	var unmarshalled []Person
+	err := json.Unmarshal([]byte(myJson), &unmarshalled)
+	if err != nil {
+		log.Println(err)
+	}
+	log.Printf("unmarshalled %v", unmarshalled)
+}

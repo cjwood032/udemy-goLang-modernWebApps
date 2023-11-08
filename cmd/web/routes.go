595dcc9c9db587938bd20f23b66552ac5c9a12f2
Chris Wood <cjwood032@gmail.com>
2023-11-07 20:21:28 -0500

    added csrf token

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -11,6 +11,7 @@ import (
 func routes(app *config.ApplConfig) http.Handler {
 	mux := chi.NewRouter()
 	mux.Use(middleware.Recoverer)
+	mux.Use(NoSurf)
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
 	return mux
--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/cjwood032/go-course
 go 1.21.3
 
 require github.com/go-chi/chi/v5 v5.0.10
+
+require github.com/justinas/nosurf v1.1.1 // indirect

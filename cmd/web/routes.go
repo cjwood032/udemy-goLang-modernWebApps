98e96606817dcd1a7b4228578ae6d7c2156c9803
Chris Wood <cjwood032@gmail.com>
2023-11-07 19:58:34 -0500

    add different routing with middleware

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,15 +1,17 @@
 package main
 
 import (
-	"github.com/bmizerany/pat"
 	"github.com/cjwood032/go-course/pkg/config"
 	"github.com/cjwood032/go-course/pkg/handlers"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"net/http"
 )
 
 func routes(app *config.ApplConfig) http.Handler {
-	mux := pat.New()
-	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+	mux := chi.NewRouter()
+	mux.Use(middleware.Recoverer)
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
 	return mux
 }
